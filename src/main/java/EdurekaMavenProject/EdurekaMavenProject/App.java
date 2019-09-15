6cda88633d5960d489f0ca0c8ceec415aa7dbbe6
Sonali Das <sonali.das@ventivtech.com>
2019-09-15 19:43:12 +0530

    first commit

---
+++ src/main/java/EdurekaMavenProject/EdurekaMavenProject/App.java
@@ -0,0 +1,13 @@
+package EdurekaMavenProject.EdurekaMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
